82995b00d984f5575458bc17422ee28110227e6c
Z1llya <zubalii.illya@gmail.com>
2022-11-21 22:05:53 +0200

    initial

--- LAB_1/src/Hello.java
+++ LAB_1/src/Hello.java
@@ -1,9 +1,10 @@
-public class Hello
-{
-public static void main(String[] args)
-{
-String message = new String();
-message = "Hello, Java";
-System.out.println(message);
+public class Hello {
+	
+ public static void main(String[] args) {
+ String message = new String();
+ message = "Illya Zubalii, KI-34";
+
+ System.out.println(message);
+ }
 }
-}
+
